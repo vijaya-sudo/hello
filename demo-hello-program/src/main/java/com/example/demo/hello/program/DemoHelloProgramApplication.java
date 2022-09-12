f5c1abea50b585a12da3038b9b55f18787947c68
vijaya-sudo <110961287+vijaya-sudo@users.noreply.github.com>
2022-09-12 07:54:43 +0530

    Add files via upload

---
+++ demo-hello-program/src/main/java/com/example/demo/hello/program/DemoHelloProgramApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.hello.program;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoHelloProgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoHelloProgramApplication.class, args);
+	}
+
+}
---
+++ demo-hello-program/src/test/java/com/example/demo/hello/program/DemoHelloProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.hello.program;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoHelloProgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
